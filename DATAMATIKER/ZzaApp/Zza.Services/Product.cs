c755a088073bdcf8b966f13598f0dbe71fc3bc69
stg3orge <cardisbunx@gmail.com>
2018-10-02 00:48:56 +0200

    adaw

---
+++ DATAMATIKER/ZzaApp/Zza.Services/Product.cs
@@ -0,0 +1,13 @@
+﻿using System.Runtime.Serialization;
+
+namespace Zza.Services
+{
+    [DataContract]
+    public class Product
+    {
+        [DataMember]
+        public int Id { get; set; }
+        [DataMember]
+        public string Name { get; set; }
+    }
+}
