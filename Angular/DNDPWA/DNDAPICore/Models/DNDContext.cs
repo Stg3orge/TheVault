597de2b24789ceac24dce3fae5b6f99b71c24363
Stg3orge <cardisbunx@gmail.com>
2019-05-03 14:15:18 +0200

    a

--- Angular/DNDPWA/DNDAPICore/Models/DNDContext.cs
+++ Angular/DNDPWA/DNDAPICore/Models/DNDContext.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DNDAPICore.Models
 {
-    public class DNDContext
+    public class DNDContext : DbContext
     {
+        public DNDContext(DbContextOptions<DNDContext> options):base(options)
+        {
+
+        }
     }
 }
