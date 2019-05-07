74205df86b5216c4bf9652d2051409d558f77762
Stg3orge <cardisbunx@gmail.com>
2019-05-07 09:29:11 +0200

    a

--- Angular/DNDPWA/DNDAPICore/Models/Player.cs
+++ Angular/DNDPWA/DNDAPICore/Models/Player.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,5 +9,14 @@ namespace DNDAPICore.Models
 {
     public class Player
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Column("nvarchar(50)")]
+        public string Username { get; set; }
+        [Column("nvarchar(50)")]
+        public string Password { get; set; }
+        [Column("nvarchar(50)")]
+        public string Name { get; set; }
     }
 }
